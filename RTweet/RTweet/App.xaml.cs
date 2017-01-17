b9789ec87de0e6815aefe00e7ebb044740e39df7
Raiti <rusuke@t-net.ne.jp>
2017-01-17 22:05:45 +0900

    通知関数の追加

--- RTweet/RTweet/App.xaml.cs
+++ RTweet/RTweet/App.xaml.cs
@@ -1,15 +1,17 @@
 ﻿using System.Windows;
+using System.Windows.Forms;
 using RTweet.Windows;
+using Application = System.Windows.Application;
 
 namespace RTweet {
 	/// <summary>
 	/// App.xaml の相互作用ロジック
 	/// </summary>
 	public partial class App : Application {
-
 		public static App AppInstance { get; private set; }
 
 		public AppNotifyIcon NotifyIcon { get; private set; }
+
 		public MainWindow AppWindow { get; private set; }
 
 		protected override void OnStartup(StartupEventArgs e) {
@@ -24,6 +26,18 @@ namespace RTweet {
 		protected override void OnExit(ExitEventArgs e) {
 			base.OnExit(e);
 			NotifyIcon.Dispose();
+		}
+
+		/// <summary>
+		/// アプリケーションの通知を起こします。
+		/// </summary>
+		/// <param name="text">通知メッセージ</param>
+		/// <param name="time">表示時間 デフォルト:500</param>
+		/// <param name="title">通知タイトル デフォルト:RTweet</param>
+		/// <param name="icon">通知メッセージアイコン デフォルト:None</param>
+		public static void Notification(string text, int time = 500, string title = "RTweet",
+			ToolTipIcon icon = ToolTipIcon.None) {
+			AppInstance.NotifyIcon.Notification(text, time, title, icon);
 		}
 
 	}
--- RTweet/RTweet/Windows/AppNotifyIcon.cs
+++ RTweet/RTweet/Windows/AppNotifyIcon.cs
@@ -1,37 +1,81 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Windows;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
 
 
 namespace RTweet.Windows {
+
+	/// <summary>
+	/// タスクトレイ駐在アイコン。通知関連
+	/// </summary>
 	public partial class AppNotifyIcon : Component {
+		
+		/// <summary>
+		/// 初期化
+		/// </summary>
 		public AppNotifyIcon() {
 			InitializeComponent();
 
 			Open.Click += OpenOnClick;
 			Exit.Click += ExitOnClick;
+			Setting.Click += SettingOnClick;
+		}
 
+
+		/// <summary>
+		/// アプリケーションの通知を起こします。
+		/// </summary>
+		/// <param name="text">通知メッセージ</param>
+		/// <param name="time">表示時間</param>
+		/// <param name="title">通知タイトル</param>
+		/// <param name="icon">通知メッセージアイコン</param>
+		public void Notification(string text, int time, string title, ToolTipIcon icon) {
+			notifyIcon.BalloonTipText = text;
+			notifyIcon.BalloonTipTitle = title;
+			notifyIcon.BalloonTipIcon = icon;
+			notifyIcon.ShowBalloonTip(time);
 		}
 
 
 
-		public AppNotifyIcon(IContainer container) {
-			container.Add(this);
 
-			InitializeComponent();
-		}
-
+		//---------------------------------------------------------------------------------------------------ここから下イベント
+		/// <summary>
+		/// アイコンをダブルクリック
+		/// </summary>
+		/// <param name="sender">sender</param>
+		/// <param name="e">event</param>
 		private void notifyIcon_DoubleClick(object sender, EventArgs e) {
 			App.AppInstance.AppWindow.Show();
 		}
 
+		/// <summary>
+		/// 設定ボタン
+		/// </summary>
+		/// <param name="sender">sender</param>
+		/// <param name="eventArgs">event</param>
+		public void SettingOnClick(object sender, EventArgs eventArgs) {
+			MessageBox.Show("未実装", "情報", MessageBoxButton.OK, MessageBoxImage.Information);
+		}
+
+		/// <summary>
+		/// アプリケーションを終了ボタン
+		/// </summary>
+		/// <param name="sender">sender</param>
+		/// <param name="eventArgs">event</param>
 		private static void ExitOnClick(object sender, EventArgs eventArgs) {
 			App.AppInstance.Shutdown();
 		}
 
+		/// <summary>
+		/// ウィンドウを開くボタン
+		/// </summary>
+		/// <param name="sender">sender</param>
+		/// <param name="e">evevt</param>
 		private static void OpenOnClick(object sender, EventArgs e) {
 			App.AppInstance.AppWindow.Show();
 		}
-
-
 	}
 }
