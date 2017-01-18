2fc9ace2668984e45c9bae4a87079edc767bfcb4
Raiti <rusuke@t-net.ne.jp>
2017-01-18 19:11:55 +0900

    すべてのエラー処理の実装

--- RTweet/RTweet/Main/Twitter/TwitterSystemPrivate.cs
+++ RTweet/RTweet/Main/Twitter/TwitterSystemPrivate.cs
@@ -27,7 +27,7 @@ namespace RTweet.Main.Twitter {
 				MessageBox.Show(@"PINコードの認証エラーが発生しました。
  PINコードが正しくないかネットワークに通ながってない可能性があります。
  詳しくはログファイルを参照してください。", @"認証エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				Instance.ErrorProcessing(e);
+				ErrorProcessing(e);
 				return null;
 			}
 		}
@@ -36,7 +36,7 @@ namespace RTweet.Main.Twitter {
 		/// 発生したTwitterAPIのエラーを処理しっます。
 		/// </summary>
 		/// <param name="exception"></param>
-		private void ErrorProcessing(TwitterException exception) {
+		private static void ErrorProcessing(TwitterException exception) {
 			Trace.WriteLine("TwitterAPIError!!");
 			Trace.WriteLine("StackTrace");
 			Trace.WriteLine(exception);
@@ -47,15 +47,67 @@ namespace RTweet.Main.Twitter {
 				switch (exceptionError.Code) {
 					case 0:
 						break;
+					case 34:
+						App.Notification("指定されたAPIが存在しません。\r\n通常起こるはずのないエラーです。");
+						break;
+					case 44:
+						App.Notification("attachment_url には引用リツイートするツイートのURLしか渡せません。");
+						break;
+					case 50:
+						App.Notification("ダイレクトメッセ―ジ指定のツイートに指定されたユーザーは存在しません。");
+						break;
+					case 67:
+						App.Notification("Twitterのバックエンドサービスがストップしています。\r\n回復までお待ちください。");
+						break;
+					case 68: 
+						App.Notification("廃止されたTwitterAPI 1.0を呼んでいます。\r\n通常起こるはずのないエラーです。");
+						break;
+					case 88:
+						Trace.WriteLine("Reset:" + exception.RateLimit.Reset.ToString("g"));
+						App.Notification("API利用制限に達しました。\r\n" + (exception.RateLimit.Reset - DateTimeOffset.Now).ToString("HH時間,mm分") + "後に再度試してください。");
+						break;
+					case 89:
+						App.Notification("Bearer tokenが正しくありません。\r\n通常起こるはずのないエラーです。");
+						break;
+					case 93:
+						App.Notification("ダイレクトメッセージのアクセスが許可されてません。\r\n通常起こるはずのないエラーです。");
+						break;
+					case 99:
+						App.Notification("OAuth2 Bearer Token の認証に失敗しました。\r\n通常起こるはずのないエラーです。");
+						break;
+					case 130:
+						App.Notification("Twitter側のエラーです。\r\nしばらく時間をおいてから再度試してしてください。");
+						break;
+					case 131:
+						App.Notification("Twitter側で原因不明のエラーが発生しました。\r\nしばらく時間をおいてから再度試してしてみてください。");
+						break;
+					case 135:
+						App.Notification("OAuthの認証に失敗しました。\r\n通常起こるはずのないエラーです。");
+						break;
+					case 150:
+						App.Notification("ダイレクトメッセージ指定のツイートが送信できませんでした。");
+						break;
+					case 185:
+						App.Notification("ツイート数の制限に達しました。\r\nしばらく時間をおいてから再度送信してみてください。");
+						break;
+					case 186:
+						App.Notification("ツイートが140字を超えました。");
+						break;
 					case 187:
 						App.Notification("ツイートが重複しています。");
+						break;
+					case 188:
+						App.Notification("添付されたURLが不正なサイトと判定されました。");
+						break;
+					case 189:
+						App.Notification("原因不明なエラーが発生しました。\r\n少し時間をおいてから再送信してみてください。");
 						break;
 					case 190:
 						App.Notification("ツイートが長すぎます。");
 						break;
 					case 191:
 						Trace.WriteLine("Reset:" + exception.RateLimit.Reset.ToString("g"));
-						App.Notification("画像送信制限に達しました。\r\n" + (exception.RateLimit.Reset - DateTimeOffset.Now).ToString("HH時間,MM分") + "後に再度送信してみてください。");
+						App.Notification("画像送信制限に達しました。\r\n" + (exception.RateLimit.Reset - DateTimeOffset.Now).ToString("HH時間,mm分") + "後に再度送信してみてください。");
 						break;
 					case 226:
 						App.Notification("アカウントがスパム判定されています。\r\nしばらく経ってから再送信してみてください。");
@@ -64,7 +116,7 @@ namespace RTweet.Main.Twitter {
 						App.Notification("アカウントがハック判定されています。\r\nメールが届いてないか確認してください。");
 						break;
 					default:
-						App.Notification("不明なエラーが発生しました\r\nエラーコード:" + exceptionError.Code + "\r\n メッセージ:" + exceptionError.Message);
+						App.Notification("不明なエラーが発生しました。\r\nエラーコード:" + exceptionError.Code + "\r\n メッセージ:" + exceptionError.Message);
 						break;
 				}
 
